33b65cc0f528e1541cfd5f701edc7b74de275408
Daniel Král <kraldaniel.cze@gmail.com>
2024-03-25 22:44:19 +0100

    Update corsMiddleware.js

--- SoudyJistotyBE/server/middlewares/corsMiddleware.js
+++ SoudyJistotyBE/server/middlewares/corsMiddleware.js
@@ -1,14 +1,26 @@
 const cors = require('cors')
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || origin === 'http://localhost:5173') {
-      callback(null, true)
-    } else {
-      callback(null, true)
-    }
-  },
+  origin: 'http://localhost:5173',
   credentials: true,
 }
 
-module.exports = cors(corsOptions)
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions
+
+  // Add your logic or condition to check the request's origin here
+  // For example, to allow any origin:
+  const allowlist = ['http://localhost:5173', 'http://example.com'] // Example allowlist
+
+  console.log('Visit from:', req.header('Origin'))
+
+  if (allowlist.indexOf(req.header('Origin')) !== -1 || !req.header('Origin')) {
+    corsOptions = { origin: true, credentials: true } // Reflect the request's origin
+  } else {
+    corsOptions = { origin: true } // Disable CORS for this request
+  }
+
+  callback(null, corsOptions) // Callback expects two parameters: error and options
+}
+
+module.exports = cors(corsOptionsDelegate)
